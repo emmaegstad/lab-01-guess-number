6d38ed740d7d419fa332834b691b53c94c7affff
Emma Egstad <emma@egstad.com>
2021-10-04 15:14:02 -0700

    Grabbed DOM elements, set-up game function

--- app.js
+++ app.js
@@ -1,8 +1,68 @@
-// import functions and grab DOM elements
+const guessInput = document.getElementById('guess-input');
+const submitButton = document.getElementById('submit-button');
+const resetButton = document.getElementById('reset-button');
+let remainingGuessText = document.getElementById('remaining-guesses');
+let resultText = document.getElementById('result-text');
 
-// initialize global state
+let hint = "";
+let remainingGuesses = 4;
+const correctNum = getRandomInt(1, 21);
+console.log(correctNum);
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+//used getRandomInt() from MDN
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+submitButton.addEventListener('click', () => {
+
+  remainingGuesses--;
+  remainingGuessText.innerText = `You have ${remainingGuesses} guesses left.`
+
+  const userGuess = Number(guessInput.value);
+
+  if (userGuess === correctNum) {
+    resultText.innerText = "Correct! You win.";
+    remainingGuessText.style.display = "none";
+    submitButton.style.display = "none";
+    resetButton.style.display = "block";
+  } else if (remainingGuesses === 0) {
+    resultText.innerText = "You lose. Out of guesses.";
+    remainingGuessText.style.display = "none";
+    submitButton.style.display = "none";
+    resetButton.style.display = "block";
+  } else if (userGuess > correctNum) {
+    hint = "Too high!";
+    resultText.innerText = `${hint} Please try again.`;
+  } else if (userGuess < correctNum) {
+    hint = "Too low!"
+    resultText.innerText = "Incorrect. Please try again.";
+    resultText.innerText = `${hint} Please try again.`;
+  }
+
+  // else(userGuess > 20) -- display Invalid guess
+
+})
+
+resetButton.addEventListener('click', () => {
+  //reset game
+})
+
+
+
+
+
+// What state do I need
+// /* * randomNum -- will be generated at the start of each game
+// * userGuess -- will hold the value of the input field
+// * numGuesses -- number of guesses -- incremented every time the user clicks the button OR starts at 4 and decrements
+// * userMessage
+//     - 'You're correct': userGuess === randomNum
+//     - 'Guess too high': userGuess > randomNum
+//     - 'Guess too low': userGuess < randomNum
+//     - 'Invalid': userGuess > 20
+//     - 'Out of guesses': numGuesses > 4/*
+
+
