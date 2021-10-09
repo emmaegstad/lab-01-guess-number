fde5b830b311b039de6c71306b9b51d953779721
fora-heims <foraheims@gmail.com>
2021-10-08 17:13:37 -0700

    Refactored work into utility functions.

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
+let timesWon = document.getElementById('win-count');
+let timesLost = document.getElementById('lose-count');
+let hint = '';
+let timesWonCount = 0;
+let timesLostCount = 0;
 const guessInput = document.getElementById('guess-input');
 const submitButton = document.getElementById('submit-button');
 const resetButton = document.getElementById('reset-button');
 let remainingGuessText = document.getElementById('remaining-guesses');
 let resultText = document.getElementById('result-text');
-let timesWon = document.getElementById('win-count');
-let timesLost = document.getElementById('lose-count');
-
-let hint = '';
 let remainingGuesses = 4;
 let correctNum = getRandomInt(1, 21);
-let timesWonCount = 0;
-let timesLostCount = 0;
 
-//used getRandomInt() from MDN
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
+}
+
+function correctGuess() {
+    timesWonCount++;
+    timesWon.innerText = `TIMES WON: ${timesWonCount}`;
+    resultText.innerText = 'Correct! You win.';
+    remainingGuessText.style.display = 'none';
+    submitButton.style.display = 'none';
+    resetButton.style.display = 'block';
+}
+
+function outOfGuesses() {
+    timesLostCount++;
+    timesLost.innerText = `TIMES LOST: ${timesLostCount}`;
+    resultText.innerText = 'No more guesses. You lose.';
+    submitButton.style.display = 'none';
+    resetButton.style.display = 'block';
+}
+
+function tooHigh() {
+    hint = 'Too high!';
+    resultText.innerText = `${hint} Try again.`;
+}
+
+function tooLow() {
+    hint = 'Too low!';
+    resultText.innerText = 'Incorrect. Please try again.';
+    resultText.innerText = `${hint} Try again.`;
 }
 
 submitButton.addEventListener('click', () => {
@@ -24,30 +50,16 @@ submitButton.addEventListener('click', () => {
     remainingGuessText.innerText = `You have ${remainingGuesses} guesses left.`;
 
     const userGuess = Number(guessInput.value);
-
     if (isNaN(userGuess) !== true) {
 
         if (userGuess === correctNum) {
-            timesWonCount++;
-            timesWon.innerText = `TIMES WON: ${timesWonCount}`;
-            resultText.innerText = 'Correct! You win.';
-            remainingGuessText.style.display = 'none';
-            submitButton.style.display = 'none';
-            resetButton.style.display = 'block';
+            correctGuess();
         } else if (remainingGuesses === 0) {
-            timesLostCount++;
-            timesLost.innerText = `TIMES LOST: ${timesLostCount}`;
-            resultText.innerText = 'No more guesses. You lose.';
-            // remainingGuessText.style.display = 'none';
-            submitButton.style.display = 'none';
-            resetButton.style.display = 'block';
+            outOfGuesses();
         } else if (userGuess > correctNum) {
-            hint = 'Too high!';
-            resultText.innerText = `${hint} Try again.`;
+            tooHigh();
         } else if (userGuess < correctNum) {
-            hint = 'Too low!';
-            resultText.innerText = 'Incorrect. Please try again.';
-            resultText.innerText = `${hint} Try again.`;
+            tooLow();
         }
     }
 });
